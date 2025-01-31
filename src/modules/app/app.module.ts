6c77a609ac3774d162d5c0ef52a7359c7990ebeb
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-01-31 12:52:16 +0330

    feat: init prisma

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,8 +1,6 @@
 import { Module, ValidationPipe } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { mikroOrmConfig } from '../../configs/mikroOrm.config';
 
 @Module({
   imports: [
@@ -10,11 +8,10 @@ import { mikroOrmConfig } from '../../configs/mikroOrm.config';
       isGlobal: true,
       envFilePath: `${process.cwd()}/.env`,
     }),
-    MikroOrmModule.forRoot(mikroOrmConfig())
   ],
   controllers: [],
   providers: [
     { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
   ],
 })
-export class AppModule {}
+export class AppModule { }
